8ff9d99c87d1d287bd47270bcff90ff5215dd0ae
Oreda-Z <shtucer31@gmail.com>
2020-09-21 17:28:03 +0300

    lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
 
-    const tasks1 = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false}
-    ]
-    const tasks2 = [
-        {id: 1, title: "Hello World!", isDone: true},
-        {id: 1, title: "I am Happy", isDone: true},
-        {id: 1, title: "Yo", isDone: true}
-    ]
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Rest API", isDone: false},
+        {id: 5, title: "GraphQL", isDone: false}
+    ]);
+
+    let [filter, setFilter] = useState<"all" | "active" | "completed">("all");
+    let tasksForTodolist = tasks;
+
+
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(t => t.isDone === false);
+    }
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(t => t.isDone === true);
+    }
+
+
+    function changeFilter(value: "all" | "active" | "completed") {
+        setFilter(value);
+    }
+    function removeTask(id: number) {
+        let filteredTasks = tasks.filter(t => t.id !== id);
+        setTasks(filteredTasks);
+    }
+
 
     return (
         <div className="App">
-            <Todolist title={"What to learn"} tasks={tasks1}/>
-            <Todolist title={"Songs"} tasks={tasks2}/>
+            <Todolist title={"What to learn"} tasks={tasksForTodolist} removeTask={removeTask} changeFilter={changeFilter}/>
         </div>
     );
 }
